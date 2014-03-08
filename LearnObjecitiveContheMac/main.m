1f827d4afbfd9b77098198835cc055a958097137
Landon CHEN <landonchen@Landons-MacBook-Pro.local>
2014-03-08 18:13:54 +0800

    Initial Commit

---
+++ LearnObjecitiveContheMac/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LearnObjecitiveContheMac
+//
+//  Created by Landon CHEN on 3/8/14.
+//  Copyright (c) 2014 Landon CHEN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
